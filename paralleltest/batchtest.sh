5f3b0762283bff51f08b3cd50625e71ee8ac2eba
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2014-08-07 10:48:59 -0400

    fixed node number

--- paralleltest/batchtest.sh
+++ paralleltest/batchtest.sh
@@ -1,8 +1,8 @@
 #!/bin/csh
-#PBS -l nodes=5:ppn=8
+#PBS -l nodes=1:ppn=8
 #PBS -q workq
-#PBS -l walltime = 00:00:01
+#PBS -l walltime=00:01:00
 #PBS -N batchtest
-cd /mnt/scratch-lustre/njones/Summer2014/paralleltest
+cd $PBS_O_WORKDIR
 module load python/2.7.6
 python motherscript.py
