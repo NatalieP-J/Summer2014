8ba4472b71c9800c509a962ab7a599bb3218018d
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2014-08-07 13:12:21 -0400

    Automated setup of WMrateget files, figured out how to kill all subprocesses started in motherscript

--- for_summer_2014/SunnyvaleWM/initializeWM.py
+++ for_summer_2014/SunnyvaleWM/initializeWM.py
@@ -1,11 +1,22 @@
 import multiprocessing
 import subprocess
 from numpy import *
+import os
+
+def makeWMfiles(rangeval):
+	for i in range(rangeval):
+		f = open('WMrateget{0}.py'.format(i+1),'wb')
+		f.write('i = {0}\n'.format(i))
+		f.close()
+		os.system('cat WMrateget.py >> WMrateget{0}.py'.format(i))
+'''
+rangeval = 8
 
 def worker(i):
 	subprocess.Popen(['python','WMrateget{0}.py'.format(i+1)])
 
 if __name__ == '__main__':
-	for i in range(8):
+	for i in range(rangeval):
 		p = multiprocessing.Process(target=worker(i))
 		p.start()
+'''
